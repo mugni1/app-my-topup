69c9316245acb4b2c6ca4d42f2c997532397fd6c
mugni1 <asepabdulmugni22@gmail.com>
2026-01-03 20:35:42 +0700

    feat: init color mode

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { useColorMode } from '@vueuse/core'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
 
+
+useColorMode()
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SuccessView from '@/views/SuccessView.vue'
+import CoView from '@/views/CoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
+    { path: '/co', name: 'co_view', component: CoView },
     { path: '/success', name: 'payment_success', component: SuccessView },
   ],
 })
