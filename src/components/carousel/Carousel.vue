96c0be218f5fe533ea5c40ff8c9b7ac3564dc72b
mugni1 <asepabdulmugni22@gmail.com>
2026-01-08 20:13:03 +0700

    refactor: improve carousel

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -2,35 +2,21 @@
   import { Swiper, SwiperSlide } from 'swiper/vue'
   import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules'
   import { ChevronLeft, ChevronRight } from 'lucide-vue-next'
+  import { data } from './data'
 
+  // init
   const modules = [Navigation, Pagination, Mousewheel, Keyboard, Autoplay]
   const navigation = {
     nextEl: '.swiper-button-next-custom',
     prevEl: '.swiper-button-prev-custom',
   }
 
-  const images = [
-    {
-      url: 'https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Fbanner%2Fweb-banner-m7passgranger-jan.webp&w=1920&q=100',
-    },
-    {
-      url: 'https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Fbanner%2Fbanner-new-year.webp&w=1920&q=100',
-    },
-    {
-      url: 'https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Fbanner%2Fweb-banner-annualstarlight-mlbb.webp&w=1920&q=100',
-    },
-    {
-      url: 'https://www.bangjeff.com/_next/image?url=https%3A%2F%2Fcdn.bangjeff.com%2Fbanner%2Fxavier-bannner.webp&w=1920&q=100',
-    },
-  ]
-
-  const onSwiper = (swiper: any) => {
-    console.log('Swiper instance:', swiper)
-  }
-
-  const onSlideChange = () => {
-    console.log('slide change')
-  }
+  // const onSwiper = (swiper: any) => {
+  //   console.log('Swiper instance:', swiper)
+  // }
+  // const onSlideChange = () => {
+  //   console.log('slide change')
+  // }
 </script>
 
 <template>
@@ -45,13 +31,11 @@
         delay: 5000,
         disableOnInteraction: false,
       }"
-      @swiper="onSwiper"
-      @slideChange="onSlideChange"
       loop
       class="rounded-md overflow-hidden shadow-md border"
     >
       <!-- SLIDE  -->
-      <SwiperSlide v-for="(image, index) in images" :key="index">
+      <SwiperSlide v-for="(image, index) in data.images" :key="index">
         <img
           class="w-full aspect-[9/3.4] object-cover object-center"
           :src="image.url"
